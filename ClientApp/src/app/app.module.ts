f632c9f49e102f6a2f6ec4a8cd2f3f33824f8932
Alim <alimzhan.alimkhanov@gmail.com>
2019-06-27 17:09:46 +0600

    создал главный экран

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http'; 
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { ValuesComponent } from './values/values.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       MainpageComponent,
-      ValuesComponent
+      ValuesComponent,
+      NavComponent,
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
    providers: [],
    bootstrap: [
